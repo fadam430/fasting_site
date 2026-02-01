da33d51bde463448189ba7a154b4e99385c01fc1
fadam430 <f.adam430@gmail.com>
2026-02-01 09:42:34 +0000

    registration changes

--- fastblog/urls.py
+++ fastblog/urls.py
@@ -1,11 +1,13 @@
 from . import views
 from django.urls import path # type: ignore
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView # type: ignore
 
 urlpatterns = [
     path('', views.FastingPlanListView.as_view(), name='fasting_list'),
     path('review/add/', views.add_review, name='add_review'),
-    path('register/', views.register, name='register'),
+    # Redirect legacy register page to django-allauth signup
+    path('register/', RedirectView.as_view(url='/accounts/signup/', permanent=False), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='fastblog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 
--- fasting/settings.py
+++ fasting/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
     # django-allauth for authentication
     'allauth',
     'allauth.account',
