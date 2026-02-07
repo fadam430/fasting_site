54dcb1f94a25c4b036f23d2b88fd39d90e94d833
fadam430 <f.adam430@gmail.com>
2026-02-07 23:54:33 +0000

    try to fix register form

--- fastblog/forms.py
+++ fastblog/forms.py
@@ -3,21 +3,60 @@ from django.contrib.auth.models import User
 
 
 class ReviewForm(forms.Form):
-    rating = forms.IntegerField(min_value=1, max_value=5, label='Rating')
-    comment = forms.CharField(widget=forms.Textarea(attrs={'rows':3}), required=False, label='Comment')
+    rating = forms.IntegerField(
+        min_value=1, 
+        max_value=5, 
+        label='Rating',
+        widget=forms.NumberInput(attrs={'class': 'form-control', 'style': 'width: 100px;'})
+    )
+    comment = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'rows': 3,
+            'class': 'form-control',
+            'placeholder': 'Share your thoughts...'
+        }), 
+        required=False, 
+        label='Comment'
+    )
 
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, min_length=8, label='Password')
-    password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'At least 8 characters'
+        }), 
+        min_length=8, 
+        label='Password'
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm your password'
+        }), 
+        label='Confirm Password'
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email']
         widgets = {
-            'email': forms.EmailInput(attrs={'placeholder': 'you@example.com'}),
-            'username': forms.TextInput(attrs={'placeholder': 'username'}),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'you@example.com'
+            }),
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Choose a username'
+            }),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Ensure all fields have Bootstrap form-control class
+        for field_name, field in self.fields.items():
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = 'form-control'
 
     def clean_password2(self):
         p1 = self.cleaned_data.get('password')
